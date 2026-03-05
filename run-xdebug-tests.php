3119044b20018e612069e4a579dd5a237d5beab9
root <root@vmi3081517.contaboserver.net>
2026-03-05 23:27:11 +0100

    Fix test harness for stripped extension
    - Fix PHP 8.6 deprecation in run-xdebug-tests.php (strpos null)
    - Replace xdebug_get_headers() with array() in shared test inc files
      (function was removed with develop/tracing modules)
    - Tests: 235/263 passing (89.4%)

--- run-xdebug-tests.php
+++ run-xdebug-tests.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+error_reporting(E_ALL & ~E_DEPRECATED);
 /*
    +----------------------------------------------------------------------+
    | Copyright (c) The PHP Group                                          |
