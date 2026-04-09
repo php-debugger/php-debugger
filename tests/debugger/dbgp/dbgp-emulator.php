596444da90531017f1d0f52f743381bec35f4d47
Carlos Granados <barel.barelon@gmail.com>
2026-04-09 19:15:26 +0200

    Changes after PR review

--- tests/debugger/dbgp/dbgp-emulator.php
+++ tests/debugger/dbgp/dbgp-emulator.php
@@ -1,6 +1,16 @@
 <?php
 require_once __DIR__ . '/dbgpclient.php';
 
+/**
+ * This can be used to emulate a DBGP client for debugging or benchmarking.
+ *
+ * Usage: php dbgp-emulator [--port=connection port] <command_list_file>
+ *
+ * When started it will wait to receive a connection from a php runner which
+ * has php-debugger enabled. Once connected, it will send to the debugger all the
+ * commands listed in the command list file. When the connection is terminated,
+ * it will exit and print the full exchange with the debugger
+ */
 class DbgpEmulator extends DebugClient
 {
 	protected $commands = [];
@@ -9,7 +19,6 @@ class DbgpEmulator extends DebugClient
 	public function __construct()
 	{
 		parent::__construct();
-		$this->setPort(9003);
 	}
 
 	public function loadCommands($filename)
@@ -132,15 +141,24 @@ if ($argc < 2) {
 }
 
 $commandFile = $argv[1];
+$port = 9003; // Default port
+
+// Parse command line options
+for ($i = 2; $i < $argc; $i++) {
+	if ($argv[$i] === '--port' && isset($argv[$i + 1])) {
+		$port = (int)$argv[$i + 1];
+		$i++; // Skip next argument since we've used it
+	}
+}
 
 $daemon = new DbgpEmulator();
+$daemon->setPort($port);
 
 if (!$daemon->loadCommands($commandFile)) {
 	exit(1);
 }
 
-echo "Starting DBGP emulator on port 9003...\n";
+echo "Starting DBGP emulator on port {$port}...\n";
 echo "Waiting for debugger connection...\n";
 
 $daemon->runEmulator();
-?>
